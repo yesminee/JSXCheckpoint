8aa0c47c14dbcde1bfc2e98dedc6be6b6fe0f41a
yesminee <43929137+yesminee@users.noreply.github.com>
2020-10-10 19:17:01 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,77 +1,28 @@
 import React from 'react';
-import './App.css';
-
+import Footer from "./Components/footer";
+import Header from "./Components/header";
+import pic from "./imageInSrc.jpg";
 
 function App() {
-  const users = [
-    { nom: "Zarrouk", prenom: "ahmed", post: "CEO" },
-    { nom: "Limam", prenom: "karima", post: "Executive director" },
-    { nom: "Bouziri", prenom: "Rached", post: "Head Master" },
-    { nom: "Mouhli", prenom: "Salma", post: "Head of research" },
-    { nom: "Kamel", prenom: "Zeineb", post: "Manager" }
-  ];
   return (
-    <div className="main">
-      <div className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="App.js">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="App.js">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="App.js">
-                Link
-              </a>
-            </li>
-          </ul>
+    <div>
+      <Header />
+      <div>
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+          <h1 className="title red" style={{ color: "red", textAlign:"center"}}>Yesmine Makkes</h1>
+          <br />
+          <img src="moi.png" alt="myimage" style={{width: "250px", height: "300px", display: "block", marginLeft: "auto",marginRight: "auto" }}/>
+          <br />
+          <img src={pic} alt="myimage" style={{ width: "250px", height: "300px", display: "block", marginLeft: "auto", marginRight: "auto" }}/>
         </div>
-      </div>
-      <h3> Notre equipe</h3>
-      <div className="bigDiv">
-        {users.map((ele) => (
-          <div>
-            <div className="card">
-              <img
-                src="imageInPublic.jpeg"
-                className="card-img-top"
-                alt="image1"
-              />
-              <div className="card-body">
-                <span className="card-title">
-                  <h5>{ele.nom + " " + ele.prenom}</h5>
-                  <br />
-                  <h6>{ele.post}</h6>
-                </span>
-              </div>
-              <a href="App.js" className="btn btn-primary">
-                plus de details
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
-      <video width="320" height="240" controls>
-        <source src="Angular.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );   
-}  
-export default App;
 
+        <video width="320" height="240" controls style={{ width: "250px", height: "300px", display: "block", marginLeft: "auto", marginRight: "auto" }}>
+          <source src="AngularCourse.mp4" type="video/mp4" />
+        </video>
+      </div>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
